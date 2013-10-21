fc95c3070d0a7f6880ec09c896c8ddbd7a340663
Carlos Rojas <carlos.rojas@adminnova.com>
2013-10-20 20:23:15 -0500

    archivos de google y logo

--- index.php
+++ index.php
@@ -27,7 +27,6 @@
 //	Set base path here so this works with symbolic links for multiple installations
 
 	define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
-	
 	require 'qa-include/qa-index.php';
 
 
